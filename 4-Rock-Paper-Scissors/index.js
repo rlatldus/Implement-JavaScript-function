4998b218e46340cfc9936bfdd57a91fce409928d
ssing_7942 <djagmlwnn12@gmail.com>
2023-06-24 20:12:47 +0900

    자바스크립트 구현 및 gif추가

--- 4-Rock-Paper-Scissors/index.js
+++ 4-Rock-Paper-Scissors/index.js
@@ -1,45 +1,34 @@
-// 무료 강의 👉 https://youtube.com/playlist?list=PLI33CnBTx2MZGD7zAQ810_B8dDU_E8gaq
-// 이 아래 코드를 작성하세요.
-// const rockButton = document
-//   .querySelector('.바위')
-//   .addEventListener('click', playGame);
-// const paperButton = document
-//   .querySelector('.보')
-//   .addEventListener('click', playGame);
-// const scissorsButton = document
-//   .querySelector('.가위')
-//   .addEventListener('click', playGame);
+const buttons = document.querySelectorAll("button");
+const resultText = document.querySelector(".result");
+const computerText = document.querySelector(".computer-choice");
+const youText = document.querySelector(".you-choice");
 
-const button = document
-  .querySelectorAll('button')
-  .addEventListener('click', (event) => {
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
     const userChoice = event.target.innerText;
     const computerChoice = () => {
-      const choices = ['바위', '보', '가위'];
+      const choices = ["바위", "보", "가위"];
       return choices[Math.floor(Math.random() * 3)];
     };
-    console.log(userChoice, computerChoice);
-    
+
     youText.textContent = userChoice;
-    resultText.textContent = (userChoice, computerChoice) => {
-      if (userChoice === computerChoice) {
-        return '비겼습니다.';
+    const computerSelection = computerChoice();
+    computerText.textContent = computerSelection;
+
+    const result = () => {
+      if (userChoice === computerSelection) {
+        return "비겼습니다.";
       } else if (
-        (userChoice === '바위' && computerChoice === '가위') ||
-        (userChoice === '보' && computerChoice === '바위') ||
-        (userChoice === '가위' && computerChoice === '보')
+        (userChoice === "바위" && computerSelection === "가위") ||
+        (userChoice === "보" && computerSelection === "바위") ||
+        (userChoice === "가위" && computerSelection === "보")
       ) {
-        return '당신이 이겼습니다!';
+        return "당신이 이겼습니다!";
       } else {
-        return '컴퓨터가 이겼습니다!';
+        return "컴퓨터가 이겼습니다!";
       }
     };
 
-    computerText.textContent = computerChoice;
+    resultText.textContent = result();
   });
-
-
-
-const resultText = document.querySelector('.result');
-const computerText = document.querySelector('.computer-choice');
-const youText = document.querySelector('.you-choice');
+});
--- 5-Modal/index.js
+++ 5-Modal/index.js
@@ -1,5 +1,3 @@
-// 무료 강의 👉 https://youtube.com/playlist?list=PLI33CnBTx2MZGD7zAQ810_B8dDU_E8gaq
-// 이 아래 코드를 작성하세요.
 const Window = (displayWindow) => {
   return (document.querySelector('.container').style.display = displayWindow);
 };
